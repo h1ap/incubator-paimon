ed33d52022b371ae8a8d605313f0157c21c7e332
heap <924952787@qq.com>
2023-07-16 16:41:05 +0800

    support flink1.17 && dinky

--- paimon-docker/start-cluster.sh
+++ paimon-docker/start-cluster.sh
@@ -4,20 +4,23 @@ echo "remove app log..."
 date_str=$(date +%Y-%m-%d)
 gsed -i "s/[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}/$date_str/g" ./data_mocker/application.yml
 
-echo "start paimon-docker env..."
-docker-compose up -d
+docker_compose_file=$1
+
+echo "start paimon-docker: $docker_compose_file env..."
+docker-compose -f $docker_compose_file up -d
 sleep 5s
 
 config_path='./data_mocker/application.yml'
 
 for i in {1..6} ; do
     if [ $i != 1 ]; then
-        docker-compose start datagen
+        echo "exec datagen..."
+        docker-compose -f $docker_compose_file start datagen
     fi
-    datagen_status=$(docker-compose ps -a datagen | awk 'NR==2')
+    datagen_status=$(docker-compose -f $docker_compose_file ps -a datagen | awk 'NR==2')
     echo "生成数据，日期：$date_str"
     while [[ $datagen_status != *"Exited"* ]]; do
-      datagen_status=$(docker-compose ps -a datagen | awk 'NR==2')
+      datagen_status=$(docker-compose -f $docker_compose_file ps -a datagen | awk 'NR==2')
       echo "wait datagen..."
       sleep 1s
     done
@@ -28,5 +31,5 @@ for i in {1..6} ; do
     gsed -i "s/mock.date: \"[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}\"/mock.date: \"$date_str\"/g" ./data_mocker/application.yml
 done
 
-docker-compose exec datanode1 bash /opt/copy_log.sh
+docker-compose -f $docker_compose_file exec datanode1 bash /opt/copy_log.sh
 echo "copy app.log to hdfs..."
--- paimon-docker/stop-cluster.sh
+++ paimon-docker/stop-cluster.sh
@@ -1,4 +1,4 @@
-docker-compose down -v
+docker-compose -f $1 down -v
 echo "stop paimon-docker env..."
 rm -f ./data_mocker/log/*
 echo "remove app log..."
